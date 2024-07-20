c8e053b8da036a641648e7b3de2d30425857a372
anjubora <ashishofficial026@gmail.com>
2024-07-20 16:36:03 +0530

    update

--- next.config.mjs
+++ next.config.mjs
@@ -9,9 +9,4 @@ const nextConfig = {
     }
 };
 
-export default nextConfig;
-module.exports = {
-    experimental: {
-      missingSuspenseWithCSRBailout: false,
-    },
-  }
+
--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,7 @@ import Footer from "@/components/footer/Footer";
 import Navbar from "@/components/navbar/Navbar";
 import { ThemeProvider } from "@/context/ThemeContext";
 import AuthProvider from "@/components/AuthProvider/AuthProvider";
+import { Suspense } from 'react'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Suspense>
         <ThemeProvider>
           <AuthProvider>
         <div className="container">
@@ -25,6 +27,7 @@ export default function RootLayout({ children }) {
         </div>
         </AuthProvider>
         </ThemeProvider>
+        </Suspense>
         </body>
     </html>
   );
