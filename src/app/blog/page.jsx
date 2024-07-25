69f9aec700b80e10531e5885bafc6b74f0135f67
anjubora <ashishofficial026@gmail.com>
2024-07-25 23:56:57 +0530

    finalupdate

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -5,7 +5,7 @@ import Image from 'next/image'
 import { notFound } from 'next/navigation';
 
 async function getData() {
-  const res = await fetch("process.env.API_URL/api/posts", {
+  const res = await fetch(`${process.env.API_URL}/api/posts`, {
     cache: "no-store",
   });
 
